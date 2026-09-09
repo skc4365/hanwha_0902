374ef4b44f26615c72a9c9964a96c893ffeb0cba
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-09-09 16:51:37 +0900

    fix 문서정리

--- day1_value_type.py
+++
@@ -1,9 +0,0 @@
-service_name: str = "summary-api"
-max_length: int = 100
-temperature: float = 0.2
-is_enabled: bool = True
-
-print(type(service_name), type(max_length))
-
-# <class 'str'> <class 'int'>
-
